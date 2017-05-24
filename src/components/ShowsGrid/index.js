146546babb716ce6fac6537f325e5bdcd59a6110
James Canning <james@jamescanning.com>
2017-05-24 13:04:45 +0100

    Group shows on show page by letter
    Currently dumb (doesn't take in to account "The" or "A"

--- src/components/ShowsGrid/index.js
+++ src/components/ShowsGrid/index.js
@@ -3,15 +3,20 @@ import groupBy from 'lodash/groupBy';
 import ShowsGridItem from './ShowsGridItem';
 
 function ShowsGrid({ shows }) {
-  const showsByLetter = groupBy(shows, show => show.name.slice[0]);
+  const showsByLetter = groupBy(shows, show => show.name[0]);
+  console.log(showsByLetter);
   return (
     <ul className="ShowsGrid">
-      <li className="ShowsGrid__group">
-        <h2>Group</h2>
-        <ul className="ShowsGrid__grid">
-          {shows.map(show => <ShowsGridItem show={show} key={show.id} />)}
-        </ul>
-      </li>
+      {Object.keys(showsByLetter).sort().map(groupKey => (
+        <li className="ShowsGrid__group">
+          <h2>{groupKey}</h2>
+          <ul className="ShowsGrid__grid">
+            {showsByLetter[groupKey].map(show => (
+              <ShowsGridItem show={show} key={show.id} />
+            ))}
+          </ul>
+        </li>
+      ))}
     </ul>
   );
 }
