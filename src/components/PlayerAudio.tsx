b74a4beb3e682a4e9bd69595a7293430bc1186d5
James Canning <james@brudil.com>
2017-10-18 12:58:41 +0100

    Fix FireFox audio caching issue

--- src/components/PlayerAudio.tsx
+++ src/components/PlayerAudio.tsx
@@ -6,8 +6,20 @@ interface IProps {
   onChange: any;
 }
 
-class PlayerAudio extends React.Component<IProps> {
+interface IState {
+  cacheKey: number
+}
+
+class PlayerAudio extends React.Component<IProps, IState> {
   private audioEl: any;
+
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      cacheKey: Math.random(),
+    };
+  }
 
   componentWillUpdate(nextProps: IProps) {
     const { userState } = this.props;
@@ -17,6 +29,7 @@ class PlayerAudio extends React.Component<IProps> {
         this.audioEl.play();
       } else {
         this.audioEl.pause();
+        this.setState({ cacheKey: Math.random() })
       }
     }
   }
@@ -28,7 +41,7 @@ class PlayerAudio extends React.Component<IProps> {
       <audio
         src={
           stream === 'live'
-            ? 'http://uk2.internet-radio.com:30764/stream'
+            ? `http://uk2.internet-radio.com:30764/stream?nocache=${this.state.cacheKey}`
             : stream
         }
         ref={ref => (this.audioEl = ref)}
