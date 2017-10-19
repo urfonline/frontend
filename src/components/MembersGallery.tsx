cdbf78c37da0c4fa83c51850bc064e8499939112
James Canning <james@brudil.com>
2017-10-19 22:50:55 +0100

    Add images, cheers Fin!

--- src/components/MembersGallery.tsx
+++ src/components/MembersGallery.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import membersOne from '../img/members_1.jpg';
+import membersTwo from '../img/members_2.jpg';
+import membersThree from '../img/members_3.jpg';
 
 function MembersGallery() {
   return (
@@ -13,13 +15,13 @@ function MembersGallery() {
         </div>
 
         <div className="MembersGallery__row MembersGallery__row--right">
-          <img className="MembersGallery__image" src={membersOne} />
-          <img className="MembersGallery__image" src={membersOne} />
+          <img className="MembersGallery__image" src={membersTwo} />
+          <img className="MembersGallery__image" src={membersTwo} />
         </div>
 
-        <div className="MembersGallery__row MembersGallery__row--left">
-          <img className="MembersGallery__image" src={membersOne} />
-          <img className="MembersGallery__image" src={membersOne} />
+        <div className="MembersGallery__row MembersGallery__row--left-faster">
+          <img className="MembersGallery__image" src={membersThree} />
+          <img className="MembersGallery__image" src={membersThree} />
         </div>
       </div>
     </div>
